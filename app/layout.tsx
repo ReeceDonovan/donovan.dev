a681e4a89373e1009163d507ab276e1a46af57d3
Reece Donovan <reecedonovan99@gmail.com>
2023-05-31 08:48:08 +0100

    Hidden the horizontal scrollbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang='en'>
       <body className={inter.className}>
-        <div className='h-screen snap-y snap-mandatory z-0 overflow-scroll'>
+        <div className='h-screen snap-y snap-mandatory z-0 overflow-y-scroll'>
           <Header />
           {children}
         </div>
