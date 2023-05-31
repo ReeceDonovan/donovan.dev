c8488b4e0799a586e8dc514156a311c6ac96ef47
Reece Donovan <reecedonovan99@gmail.com>
2023-05-31 12:27:06 +0100

    Updated header component to use framer motion animations

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,16 @@
 import { SocialIcon } from 'react-social-icons';
+import * as motion from '@/lib/motion';
 
 export default function Header() {
   return (
     <header className='sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center'>
       {/* TODO: Make social icons dynamically generated */}
-
-      <div className='flex flex-row items-center'>
+      <motion.div
+        initial={{ x: -500, opacity: 0, scale: 0.5 }}
+        animate={{ x: 0, opacity: 1, scale: 1 }}
+        transition={{ duration: 1.25 }}
+        className='flex flex-row items-center'
+      >
         {/* Social Icons */}
         <SocialIcon
           network='github'
@@ -25,10 +30,15 @@ export default function Header() {
           fgColor='gray'
           bgColor='transparent'
         />
-      </div>
+      </motion.div>
 
       {/* Contact Icons */}
-      <div className='flex flex-row items-center text-gray-300 cursor-pointer'>
+      <motion.div
+        initial={{ x: 500, opacity: 0, scale: 0.5 }}
+        animate={{ x: 0, opacity: 1, scale: 1 }}
+        transition={{ duration: 1.25 }}
+        className='flex flex-row items-center text-gray-300 cursor-pointer'
+      >
         <SocialIcon
           className='cursor-pointer'
           network='email'
@@ -38,7 +48,7 @@ export default function Header() {
         <p className='uppercase hidden md:inline-flex text-sm text-gray-400'>
           Contact Me
         </p>
-      </div>
+      </motion.div>
     </header>
   );
 }
