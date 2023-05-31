cdc02b8dd3bb9f134c0138b3d354abe5e0ad1181
Reece Donovan <reecedonovan99@gmail.com>
2023-05-31 08:40:32 +0100

    Implemented Hero component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
-import Image from 'next/image';
+import Hero from '@/components/Hero';
 
 export default function Home() {
   return (
-    <div>
-      {/* TODO: Placeholder Homepage Content */}
-      <h1>Welcome to Reece Donovan&rsquo;s personal website</h1>
+    <div className='h-screen'>
+      {/* Hero */}
+      <section id='hero' className='snap-center'>
+        <Hero />
+      </section>
     </div>
   );
 }
---
+++ components/Hero.tsx
@@ -0,0 +1,58 @@
+'use client';
+
+import { Cursor, useTypewriter } from 'react-simple-typewriter';
+import BackgroundCircles from './BackgroundCircles';
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function Hero() {
+  const [text, count] = useTypewriter({
+    words: [
+      "Hi, I'm Reece Donovan",
+      'Hobbyist Photographer',
+      'Caffeine Worshipper',
+      'Jack of All Trades',
+      '...Master of None',
+    ],
+    loop: true,
+    delaySpeed: 2000,
+  });
+
+  return (
+    <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'>
+      <BackgroundCircles />
+      {/* TODO: Replace GitHub Avatar with a proper photo */}
+      <Image
+        src='https://avatars.githubusercontent.com/u/42524740?v=4'
+        alt='Reece Donovan - GitHub Avatar'
+        className='relative rounded-full h-32 w-32 mx-auto object-cover'
+        width={128}
+        height={128}
+      />
+      <div className='z-20'>
+        <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[15px]'>
+          Software Engineer
+        </h2>
+        <h1 className='text-5xl lg:text-6xl font-semibold px-10'>
+          <span className='mr-3'>{text}</span>
+          <Cursor cursorColor='#F7AB0A' />
+        </h1>
+
+        <div className='pt-5'>
+          <Link href='#about'>
+            <button className='heroButton'>About</button>
+          </Link>
+          <Link href='#experience'>
+            <button className='heroButton'>Experience</button>
+          </Link>
+          <Link href='#skills'>
+            <button className='heroButton'>Skills</button>
+          </Link>
+          <Link href='#projects'>
+            <button className='heroButton'>Projects</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
