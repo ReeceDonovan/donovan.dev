2fd9f084c9b5c6259e13d39b6c0cc70831157ee3
Reece Donovan <reecedonovan99@gmail.com>
2023-06-02 19:00:44 +0100

    Implemented data fetching and prop tunnelling on root

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from 'next';
+import { groq } from 'next-sanity';
 
 import About from '@/components/About';
 import ContactMe from '@/components/ContactMe';
@@ -7,6 +8,8 @@ import Hero from '@/components/Hero';
 import Projects from '@/components/Projects';
 import Skills from '@/components/Skills';
 import WorkExperience from '@/components/WorkExperience';
+import { client } from '@/sanity/lib/client';
+import { Experience, PageInfo, Project, Skill, Social } from '@/typings';
 import { ArrowUpCircleIcon } from '@heroicons/react/24/outline';
 
 export const metadata: Metadata = {
@@ -15,28 +18,44 @@ export const metadata: Metadata = {
 };
 
 /* TODO: Rename the homepage section components.e.g. "Hero.tsx" -> "HeroSection.tsx" */
-export default function Home() {
+export default async function Home() {
+  const experienceQuery = groq`*[_type == "experience"] {..., technologies[]->}`;
+  const pageInfoQuery = groq`*[_type == "pageInfo"][0]`;
+  const projectsQuery = groq`*[_type == "project"] {..., technologies[]->}`;
+  const skillsQuery = groq`*[_type == "skill"]`;
+  const socialsQuery = groq`*[_type == "social"]`;
+
+  const experiences: Experience[] = await client.fetch(experienceQuery);
+
+  const pageInfo: PageInfo = await client.fetch(pageInfoQuery);
+
+  const projects: Project[] = await client.fetch(projectsQuery);
+
+  const skills: Skill[] = await client.fetch(skillsQuery);
+
+  const socials: Social[] = await client.fetch(socialsQuery);
+
   return (
     <div className='h-screen snap-y snap-mandatory z-0 overflow-x-hidden overflow-y-scroll scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/70'>
-      <Header />
+      <Header socials={socials} />
       <section id='hero' className='snap-start'>
-        <Hero />
+        <Hero pageInfo={pageInfo} />
       </section>
 
       <section id='about' className='snap-center'>
-        <About />
+        <About pageInfo={pageInfo} />
       </section>
 
       <section id='experience' className='snap-center'>
-        <WorkExperience />
+        <WorkExperience experiences={experiences} />
       </section>
 
       <section id='skills' className='snap-start'>
-        <Skills />
+        <Skills skills={skills} />
       </section>
 
       <section id='projects' className='snap-start'>
-        <Projects />
+        <Projects projects={projects} />
       </section>
 
       {/* TODO: Rename the "ContactMe" component */}
