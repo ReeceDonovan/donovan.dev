4077c52dd8bc81a2ca35315cb15b269de062d446
Reece Donovan <reecedonovan99@gmail.com>
2023-06-02 06:54:31 +0100

    Fixed skill item hover bug

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -19,12 +19,17 @@ export default function Skill({ directionLeft }: SkillProps) {
         className='rounded-full border border-gray-500 object-cover w-24 h-24 md:w-28 md:h-28 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out'
       />
 
-      <div className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0'>
+      <motion.div
+        initial={{ x: directionLeft ? -200 : 200 }}
+        whileInView={{ x: 0 }}
+        transition={{ duration: 1 }}
+        className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0'
+      >
         <div className='flex items-center justify-center h-full'>
           {/* TODO: Populate with real dynamic data */}
           <p className='text-3xl font-bold text-black opacity-100'>100%</p>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 }
