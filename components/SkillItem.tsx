bc3b579b65a6d85c01dd2a45422745b545b7b2e0
Reece Donovan <reecedonovan99@gmail.com>
2023-06-04 17:16:13 +0100

    Removed buggy mobile animation from skills items

--- components/SkillItem.tsx
+++ components/SkillItem.tsx
@@ -1,38 +1,30 @@
-import * as motion from '@/lib/motion';
+import Image from 'next/image';
+
 import { urlForImage } from '@/sanity/lib/image';
 import { Skill } from '@/typings';
 
 interface SkillProps {
-  // Determines if the skill should animate from the left or right.
-  // If true, it animates from left. If false or undefined, it animates from right.
-  directionLeft?: boolean;
   skill: Skill;
 }
 
-export default function SkillItem({ directionLeft, skill }: SkillProps) {
+export default function SkillItem({ skill }: SkillProps) {
   return (
     <div className='group relative flex cursor-pointer'>
-      <motion.img
-        initial={{ x: directionLeft ? -200 : 200, opacity: 0 }}
-        whileInView={{ x: 0, opacity: 1 }}
-        transition={{ duration: 1 }}
-        src={urlForImage(skill?.image).url()}
+      <Image
+        src={urlForImage(skill?.image).size(256, 256).fit('min').pad(10).url()}
         alt={skill?.title ? `${skill.title} Logo` : 'Logo'}
+        width={128}
+        height={128}
         className='rounded-full border border-gray-500 object-cover w-24 h-24 md:w-28 md:h-28 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out'
       />
 
-      <motion.div
-        initial={{ x: directionLeft ? -200 : 200 }}
-        whileInView={{ x: 0 }}
-        transition={{ duration: 1 }}
-        className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0'
-      >
+      <div className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0 max-w-full'>
         <div className='flex items-center justify-center h-full'>
           <p className='text-3xl font-bold text-black opacity-100'>
             {skill?.progress ?? '0'}%
           </p>
         </div>
-      </motion.div>
+      </div>
     </div>
   );
 }
--- components/SkillSection.tsx
+++ components/SkillSection.tsx
@@ -13,7 +13,7 @@ export default function SkillSection({ skills }: Props) {
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       transition={{ duration: 1.5 }}
-      className='flex relative flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center'
+      className='flex relative flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-[calc(100dvh)] justify-center xl:space-y-0 mx-auto items-center'
     >
       <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
         Skills
@@ -23,14 +23,12 @@ export default function SkillSection({ skills }: Props) {
         Hover over a skill for current experience level
       </h3>
 
-      <div className='grid grid-cols-4 gap-5'>
-        {skills?.map((skill, idx) => (
-          <SkillItem
-            key={skill._id}
-            skill={skill}
-            directionLeft={idx >= skills.length / 2}
-          />
-        ))}
+      <div className='grid grid-cols-4 gap-5 px-2'>
+        {skills
+          ?.sort((a, b) => b.progress - a.progress)
+          .map((skill) => (
+            <SkillItem key={skill._id} skill={skill} />
+          ))}
       </div>
     </motion.div>
   );
