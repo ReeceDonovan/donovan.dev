a33032c21e6ce5b1e03ed332700e34db09f681be
Reece Donovan <reecedonovan99@gmail.com>
2023-06-05 00:21:10 +0100

    Updated buggy Experience styling on mobile

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -10,44 +10,55 @@ type Props = {
 
 export default function ExperienceCard({ experience }: Props) {
   return (
-    <article className='flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-[#292929] p-10 hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 overflow-hidden'>
+    <article className='flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[calc(100dvw-5rem)] md:w-[600px] xl:w-[900px] snap-center bg-[#292929] p-10 hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 overflow-hidden'>
       <motion.img
         initial={{ y: -100, opacity: 0 }}
         whileInView={{ y: 0, opacity: 1 }}
         transition={{ duration: 1.25 }}
         viewport={{ once: true }}
-        className='w-32 h-32 rounded-full xl:w-[200px] xl:h-[200px] object-cover object-center'
+        className='w-28 h-28 md:w-32 md:h-32 rounded-full xl:w-[200px] xl:h-[200px] object-cover object-center -mb-3 md:mb-0 -mt-5 md:mt-0'
         src={urlForImage(experience?.companyImage).url() ?? ''}
         alt={
           experience?.company ? `${experience.company} Logo` : 'Company Logo'
         }
       />
 
-      <div className='px-0 md:px-10'>
-        <h4 className='text-4xl font-light'>{experience?.jobTitle}</h4>
-        <p className='font-bold text-2xl mt-1'>{experience?.company}</p>
+      <div className='px-0 md:px-10 text-center md:text-left'>
+        <h4 className='text-xl md:text-4xl font-light'>
+          {experience?.jobTitle}
+        </h4>
+        <p className='font-bold text-base md:text-2xl mt-1'>
+          {experience?.company}
+        </p>
 
-        <div className='flex space-x-2 my-2'>
-          {experience?.technologies?.map((tech) => (
-            <Image
-              key={tech._id}
-              width={40}
-              height={40}
-              className='h-10 w-10 rounded-full'
-              src={urlForImage(tech?.image).url() ?? ''}
-              alt={tech?.title ? `${tech.title} Logo` : 'Technology Logo'}
-            />
-          ))}
-        </div>
-
-        <p className='uppercase py-5 text-gray-300'>
+        <p className='uppercase py-3 md:py-5 text-gray-300 hover:text-primary/60 transition-all duration-300 text-base'>
           {new Date(experience?.dateStarted).toLocaleDateString('en-IE')} -{' '}
           {experience.isCurrentPosition
             ? 'Present'
             : new Date(experience?.dateEnded).toLocaleDateString('en-IE')}
         </p>
 
-        <ul className='list-disc space-y-4 ml-5 text-lg'>
+        <div className='flex flex-wrap justify-center md:justify-start space-x-2 md:space-x-4 mb-2'>
+          {experience?.technologies?.map((tech) => (
+            <Image
+              key={tech._id}
+              width={40}
+              height={40}
+              className='h-7 w-7 md:h-10 md:w-10 rounded-full p-0.5'
+              src={
+                urlForImage(tech?.image)
+                  .size(128, 128)
+                  .fit('min')
+                  .quality(3)
+                  .blur(10)
+                  .url() ?? ''
+              }
+              alt={tech?.title ? `${tech.title} Logo` : 'Technology Logo'}
+            />
+          ))}
+        </div>
+
+        <ul className='list-disc space-y-2 md:space-y-4 ml-5 text-base md:text-lg text-left mt-5'>
           {experience?.points?.map((point, idx) => (
             <li key={idx}>{point}</li>
           ))}
--- components/ExperienceSection.tsx
+++ components/ExperienceSection.tsx
@@ -13,13 +13,13 @@ export default function ExperienceSection({ experiences }: Props) {
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       transition={{ duration: 1.5 }}
-      className='h-screen flex relative overflow-hidden flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center'
+      className='h-[calc(100dvh)] flex relative overflow-hidden flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center'
     >
-      <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
-        Work Experience
+      <h3 className='absolute top-20 md:top-24 uppercase tracking-[10px] md:tracking-[20px] text-gray-500 text-xl md:text-2xl pl-3'>
+        Experience
       </h3>
 
-      <div className='w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-primary/70'>
+      <div className='w-full flex space-x-5 overflow-x-scroll p-5 pt-16 md:p-10 snap-x snap-mandatory scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-primary/70 h-[calc(100dvh-7rem)] md:h-fit '>
         {experiences?.map((experience) => (
           <ExperienceCard key={experience._id} experience={experience} />
         ))}
