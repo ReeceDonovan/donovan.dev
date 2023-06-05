ace5769999c6a8f278a0abdce4edc4311a1255e0
Reece Donovan <reecedonovan99@gmail.com>
2023-06-05 13:28:02 +0100

    Updated mobile styling for skills items

--- components/SkillItem.tsx
+++ components/SkillItem.tsx
@@ -9,18 +9,18 @@ interface SkillProps {
 
 export default function SkillItem({ skill }: SkillProps) {
   return (
-    <div className='group relative flex cursor-pointer'>
+    <div className='group relative flex cursor-pointer w-fit'>
       <Image
         src={urlForImage(skill?.image).size(256, 256).fit('min').pad(10).url()}
         alt={skill?.title ? `${skill.title} Logo` : 'Logo'}
         width={128}
         height={128}
-        className='rounded-full border border-gray-500 object-cover w-24 h-24 md:w-28 md:h-28 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out'
+        className='rounded-full border border-gray-500 object-cover w-24 h-24 md:w-28 md:h-28 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out '
       />
 
-      <div className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0 max-w-full'>
-        <div className='flex items-center justify-center h-full'>
-          <p className='text-3xl font-bold text-black opacity-100'>
+      <div className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0 max-h-full max-w-full'>
+        <div className='flex items-center justify-center h-full w-full'>
+          <p className='text-3xl font-bold text-black opacity-100 min-w-24'>
             {skill?.progress ?? '0'}%
           </p>
         </div>
--- components/SkillSection.tsx
+++ components/SkillSection.tsx
@@ -13,22 +13,23 @@ export default function SkillSection({ skills }: Props) {
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       transition={{ duration: 1.5 }}
-      className='flex relative flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-[calc(100dvh)] justify-center xl:space-y-0 mx-auto items-center'
+      className='h-[calc(100dvh)] flex relative flex-col text-center items-center justify-center'
     >
-      <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
+      <h3 className='absolute top-20 md:top-24 uppercase tracking-[10px] md:tracking-[20px] text-gray-500 text-xl md:text-2xl pl-3'>
         Skills
       </h3>
-
-      <h3 className='absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm'>
+      <h3 className='absolute top-28 mt-1 md:top-36 uppercase tracking-[3px] text-gray-500 text-xs md:text-sm'>
         Hover over a skill for current experience level
       </h3>
 
-      <div className='grid grid-cols-4 gap-5 px-2'>
-        {skills
-          ?.sort((a, b) => b.progress - a.progress)
-          .map((skill) => (
-            <SkillItem key={skill._id} skill={skill} />
-          ))}
+      <div className='overflow-x-hidden h-[calc(100dvh*0.6)] w-[calc(100dvw-5rem)] md:w-[600px] xl:w-[900px] mt-12 py-5 md:p-10 scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-primary/60'>
+        <div className='flex flex-row flex-wrap flex-shrink-0 justify-evenly items-center space-y-2 py-2 w-fit self-center'>
+          {skills
+            ?.sort((a, b) => b.progress - a.progress)
+            .map((skill) => (
+              <SkillItem key={skill._id} skill={skill} />
+            ))}
+        </div>
       </div>
     </motion.div>
   );
